a85e8bef13314d1c9e12393e5fcae79d2d9b04e8
Allan Wylker <allan.wylker14@gmail.com>
2023-04-07 13:11:38 -0300

    Rotas da página Reservas

--- server/routes/reservas.js
+++ server/routes/reservas.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { controllerShowReservas } from '../controller/reservas.js';
+import { controllerShowReservas, controllerShowReservasById, controllerInsertReserva, controllerModifyReserva, controllerDeleteReserva } from '../controller/reservas.js';
 
 const router = express.Router()
 
 router.get('/reservas', controllerShowReservas)
+router.get('/reservas/:id', controllerShowReservasById)
+
+router.post('/reservas', controllerInsertReserva)
+router.put('/reservas/:id', controllerModifyReserva)
+router.delete('/reservas/:id', controllerDeleteReserva)
 
 export default router
