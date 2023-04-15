dc1d936d03bbe1e82e095823fe8e0942e39cdcb2
Sylvia Xavier <Xavier_sylvia@hotmail.com>
2023-04-15 20:35:08 -0300

    adiciona card minhas reservas

--- client/src/views/MinhasReservasView.vue
+++ client/src/views/MinhasReservasView.vue
@@ -3,17 +3,31 @@
   <main>
     <h1>Minhas Reservas</h1>
     <section>
-        <p class ="centro" id="quarto">Quarto: </p>
-        <p class="centro" id="chekin">Data do Chek-in: </p>
-        <p class="centro" id="chekout">Data do Check-out: </p>
-        <p class="centro" id="pessoas">Número de hóspedes: </p>
-        <p class="centro" id="totalReserva">Número de hóspedes: </p>
-        <p class="centro" id="totalReserva">Valor total da reserva: </p>
-        <p class ="centro" id="quarto">Serviços Adicionais: </p>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_content">
+
+                    <p id="quartoResumo" class="card_title">Acomodação: </p>
+                    <p id="checkinResumo" class="card_text">Data do Check-in: </p>
+                    <p id="checkoutResumo" class="card_text">Data de Check-out: </p>
+                    <p id="pessoasResumo" class="card_text">Número de hóspedes: </p>
+                    <p class="card_text">Serviços Adicionais: </p>
+                    <div class="resumo-div">
+                        <p id="servicos"></p>
+                        <p id="servicos0"></p>
+                        <p id="servicos1"></p>
+                        <p id="servicos2"></p>
+                        <p id="servicos3"></p>
+                        <p id="servicos4"></p>
+                    </div>
+
+                    <p id="totalResumo" class="card_text">Valor total da reserva: </p>
+                </div>
+            </div>
+        </li>
     </section>
   </main>
 </body>
-
 </template>
 
 <script>
@@ -23,6 +37,7 @@ export default {
       return {
       };
     },
+    
     methods: {
       pegarData(data) {
         data = data.replaceAll(/-/g, '')
@@ -31,7 +46,45 @@ export default {
         let dia = (data.slice(6, 8))
         return `${dia}/${mes}/${ano}`
       },
+      ResumoMinhasReservas() {
+        
+        let subtotal = parseFloat(localStorage.getItem('valorServiço: '))
+            subtotal += parseFloat(localStorage.getItem('Total'))
 
+        document.querySelector("#totalResumo").textContent =
+            "Valor Total da Reserva: " + this.formatarDinheiro(subtotal)
+
+        document.querySelector("#quartoResumo").textContent =
+            localStorage.getItem("nomeQuarto");
+       
+        document.querySelector("#checkinResumo").textContent =
+            "Data do Check-in: " + this.pegarData(localStorage.getItem("checkin"));
+
+        document.querySelector("#checkoutResumo").textContent =
+            "Data do Check-out: " + this.pegarData(localStorage.getItem("checkout"));
+            
+            
+        document.querySelector("#pessoasResumo").textContent =
+            "Número de hóspedes: " + localStorage.getItem("qtdePessoas");
+
+        document.querySelector("#servicos").textContent = 
+            "Serviços Adicionais: "
+
+        document.querySelector("#servicos0").textContent = 
+            localStorage.getItem("servicoNome0")
+
+        document.querySelector("#servicos1").textContent = 
+            localStorage.getItem("servicoNome1")
+
+        document.querySelector("#servicos2").textContent = 
+            localStorage.getItem("servicoNome2")
+
+        document.querySelector("#servicos3").textContent = 
+            localStorage.getItem("servicoNome3")
+            
+        document.querySelector("#servicos4").textContent = 
+            localStorage.getItem("servicoNome4")
+    },/*
 	    getdata() {
 		    document.querySelector('#quarto').textContent = "Quarto: " + localStorage.getItem('nomeQuarto');
 
@@ -42,7 +95,7 @@ export default {
 		    document.querySelector('#pessoasModalResumo').textContent = "Número de hóspedes: " + localStorage.getItem('qtdePessoas')
            
         document.querySelector('#totalReserva').textContent = "Valor total da reserva:" + localStorage.getItem('totalReserva');
-	    }
+	    }*/
     }
 }
 
@@ -50,129 +103,42 @@ export default {
 </script>
 
 <style>
-
-.centro {
-  text-align: center;
-}
-.modal-backdrop {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.7);
+.cards {
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
-
-.modal {
-  background-color: #fff;
-  box-shadow: 2px 2px 20px 1px;
-  padding: 0 1rem;
+.cards_item {
+  display: flex;
+  padding: 1rem;
+  width: 40%;
+}
+.card {
+  border-radius: 0.25rem;
+  box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
   display: flex;
   flex-direction: column;
-  margin-top: 5%;
-  margin-left: 25%;
-  width: 50%;
-  height: 80%;
-  text-align: center;
-  overflow-x: hidden;
-}
-
-.modal-header {
-  padding: 15px;
-  display: flex;
-}
-
-.modal-header {
-  position: relative;
-  border-bottom: 1px solid #e0e0e0;
-  color: #161414;
-  justify-content: space-between;
-}
-.modal-header h2 {
-  text-align: center;
-  width: 90%;
-}
-
-.res-serv {
-  text-align: center;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-top: 1em;
-  justify-content: space-evenly;
-
-}
-.res-serv2 {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-
-}
-.modal-body {
-  position: relative;
-  padding: 20px 10px;
   overflow: hidden;
+  width: 60%;
 }
-
-.button-wrap {
-  margin: 1em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+.card_content {
+  padding: 1rem;
+  background-color:#e4d8d8 ;
 }
-.button {
-  display: flex;
-  padding: 1.5em 2.5em;
-  align-items: center;
-  text-align: center;
-  flex: 0 0 auto;
-  text-decoration: none;
-  text-transform: uppercase;
-  cursor: pointer;
-  border: 1px solid #e0e0e0;
-  justify-content: center;
-  max-width: 25%;
-  transition: 0.5s;
-}
-.btn-close {
-  position: absolute;
-  top: 0;
-  right: 0;
-  border: none;
-  font-size: xx-large;
-  padding: 10px;
-  cursor: pointer;
-  font-weight: bold;
-  color: #c0c0c0;
-}
-.button:hover {
-  border-color: #c0c0c0;
-}
-.button-text {
+.card_title {
   color: #333333;
+  font-size: large;
+  font-weight: normal;
+  text-transform: capitalize;
   text-align: center;
-  display: block;
-  text-align: center;
+  margin: 0px;
 }
-.button-arrow {
-  display: inline;
-  transition: 0.5s;
-  margin: 0.3%;
-}
-.button:hover .button-arrow {
-  transform: translate3d(1em, 0, 0);
-}
-
-.modal-fade-enter,
-.modal-fade-leave-to {
-  opacity: 0;
-}
-
-.modal-fade-enter-active,
-.modal-fade-leave-active {
-  transition: opacity 0.5s ease;
+.card_text {
+  color: #333333;
+  font-weight: normal;
+  font-size: medium;
+  margin-bottom: 1.25rem;
 }
 </style>
